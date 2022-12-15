08410eb20adf6951031178c92c24d876d28a7ce9
Perrin <perrinletembet.luquet@gmail.com>
2022-12-15 08:09:43 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,6 +5,7 @@
  * Return: 0
 */
 
+
 int main(void)
 
 {
@@ -14,9 +15,9 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/*while (i < 10)*/
-	/*(*/
+	/*{*/
 	/*      putchar(i);*/
-	/*)*/
+	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
