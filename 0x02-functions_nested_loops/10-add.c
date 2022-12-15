75d21d36436f3e35368b037141d9619a99400d25
Perrin <perrinletembet.luquet@gmail.com>
2022-12-14 17:48:52 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: The first number to be captured
+ * @n2: The second number to be captured
+ * Return: Integer value
+*/
+
+int add(int n1, int n2)
+{
+
+	return (n1 + n2);
+}
