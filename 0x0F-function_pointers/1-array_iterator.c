1e803749a3c84e1b56a4dd39de2437a9b445dbb1
Perrin <perrinletembet.luquet@gmail.com>
2023-01-22 14:21:22 -0800

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * array_iterator - function that executes function given as param.
  * @array: array of elements.
@@ -7,10 +8,11 @@
  *
  * Return: void.
 */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	
+
 	if (array && size > 0 && action)
 	{
 		for (i = 0; i < size; i++)
