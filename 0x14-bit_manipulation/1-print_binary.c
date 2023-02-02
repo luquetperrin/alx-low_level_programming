b5975ec3485f6a4b2e105a1431652e1973a30c29
Perrin <perrinletembet.luquet@gmail.com>
2023-02-02 04:11:07 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation
- * of a number.
- * @n: unsigned long int.
- *
- * Return: no return.
+ * print_binary - prints the binary representation of a number.
+ * @n: The number to be printed in binary.
 */
 void print_binary(unsigned long int n)
 {
