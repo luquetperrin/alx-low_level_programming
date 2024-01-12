b8a16292a4affe9e42e31bae1ff929d3fff5a48b
Perrin <perrinletembet.luquet@gmail.com>
2024-01-11 16:33:08 -0800

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * factorial - Calculate the factorial of a numbe
- * @n: the neber
+ * _strlen_recursion - Calculate the lengthvof a string
+ * @s: the string to count
  *
- * Return: Integer value
+ * Return: integer value
  */
-int factorial(int n)
+int _strlen_recursion(char *s)
 {
-if (n < 0)
-return (-1);
-if  (n <= 1)
-return (1);
-
-return (n * factorial(n - 1));
+if (*s == '\0')
+{
+return (0);
+}
+s++;
+return (_strlen_recursion(s) + 1);
 }
