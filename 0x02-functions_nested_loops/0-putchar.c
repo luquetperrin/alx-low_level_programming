0849e9087a31a82628a756f50261b3070075c78e
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-07-23 07:09:39 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
+
 /**
- * main - Entry point
+ * main - prints _putchar followed by a new line
  *
- * Return: Always success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+    char *str = "_putchar";
+    int i = 0;
 
-printf("_putchar\n");
+    while (str[i])
+    {
+        _putchar(str[i]);
+        i++;
+    }
+    _putchar('\n');
 
-return (0);
+    return (0);
 }
+
