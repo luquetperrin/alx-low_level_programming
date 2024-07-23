6492baf9a05c2bf9895eafdc9ef4a6af5e54a645
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-07-23 07:44:52 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Add two intigers
+ * @num1: first intiger
+ * @num2: secod intiger
+ * Return: Always success
+ */
+int add(int num1, int num2)
+{
+
+return (num1 + num2);
+}
